3f468d4bf2237a6d377fb8b867b6fed7058735cb
GrantZheng86 <33077268+GrantZheng86@users.noreply.github.com>
2021-12-20 22:47:50 -0700

    Finished regression

--- Needle_with_BG/ColorPicker.py
+++ Needle_with_BG/ColorPicker.py
@@ -1,6 +1,9 @@
+import argparse
+
 import cv2
 import numpy as np
 from tkinter import *
+
 
 class WrappedImage:
 
@@ -53,10 +56,207 @@ class WrappedImage:
             raise Exception("Invalid Color Space")
         self._image = cv2.cvtColor(self._image, cv2.COLOR_BGR2GRAY)
 
-    def color_filter_HSV(self, hue, saturation, value):
+    def color_filter_HSV(self, upper_bound, lower_bound):
         if self._color_space != "HSV":
             self.change_to_HSV()
 
+        if upper_bound[0] < lower_bound[1]:
+            upper_bound_floor_hue = 0
+            lower_bound_celling_hue = 180
+
+            upper_bound_floor = (upper_bound_floor_hue, lower_bound[1], lower_bound[2])
+            lower_bound_celling = (lower_bound_celling_hue, upper_bound[1], upper_bound[2])
+            mask_1 = cv2.inRange(self._image, upper_bound_floor, upper_bound)
+            mask_2 = cv2.inRange(self._image, lower_bound, lower_bound_celling)
+            mask = cv2.bitwise_or(mask_1, mask_2)
+        else:
+            mask = cv2.inRange(self._image, lower_bound, upper_bound)
+
+        kernel = np.ones((5, 5), np.uint8)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+        mask = WrappedImage.process_mask(mask, 6)
+        bounding_rect_list = WrappedImage.find_bounding_rect(mask)
+        bounding_ellipse_list = WrappedImage.find_bounding_ellipse(mask)
+        marker_points = WrappedImage.find_end_locations(bounding_rect_list)
+        masked_image = cv2.bitwise_and(self._image, self._image, mask=mask)
+        masked_image = cv2.cvtColor(masked_image, cv2.COLOR_HSV2BGR)
+        # masked_image = self.draw_bounding_rect_on_image(bounding_rect_list)
+        masked_image = self.draw_bounding_ellipse_on_image(bounding_ellipse_list)
+
+        return masked_image, marker_points
+
+    def draw_bounding_rect_on_image(self, rectangles):
+
+        if self._color_space != "BGR":
+            self.change_to_BGR()
+
+        to_show = self.get_image().copy()
+
+        for rectangle in rectangles:
+            to_show = cv2.drawContours(to_show, [np.array(rectangle)], 0, (255, 0, 0), 2)
+
+        return to_show
+
+    def draw_bounding_ellipse_on_image(self, ellipses):
+        if self._color_space != "BGR":
+            self.change_to_BGR()
+
+        to_show = self.get_image().copy()
+
+        for ellipse in ellipses:
+            to_show = cv2.ellipse(to_show, ellipse, (255, 0, 0), 2)
+
+        return to_show
+
+
+    def draw_marker_on_image(self, markers):
+        if self._color_space != "BGR":
+            self.change_to_BGR()
+
+        to_draw = self.get_image().copy()
+
+        for marker in markers:
+            left = marker[0]
+            right = marker[1]
+            to_draw = cv2.circle(to_draw, center=left, radius=4, color=(0, 255, 0), thickness=2)
+            to_draw = cv2.circle(to_draw, center=right, radius=4, color=(0, 255, 0), thickness=2)
+
+        cv2.imshow('Marker Ends', to_draw)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def show_image_gradient(self):
+
+        laplacian = cv2.Laplacian(self._image, 8)
+        cv2.imshow('Laplacian', laplacian)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def show_hue_only(self):
+        if self.get_colo_space() != "HSV":
+            self.change_to_HSV()
+
+        hue_layer = self._image[:, :, 0]
+        cv2.imshow('Hue Only', hue_layer)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    @staticmethod
+    def process_mask(mask, marker_count):
+        """
+        Filters out smaller, non-marker masked area
+        :param mask: The original mask
+        :param marker_count: How many markers are there on the needle
+        :return:
+        """
+        retval, labels, stats, _ = cv2.connectedComponentsWithStats(mask, 8, cv2.CV_32S)
+        # Stat is [left, top, width, height, area]
+        stats = stats.tolist()
+        for i in range(len(stats)):
+            stats[i].append(i)
+
+        stats.sort(key=lambda x: x[-2])
+        useful_stats = stats[-1 - marker_count: -1]  # The actual red markers on needle
+        useful_labels = np.array(useful_stats)[:, -1]
+
+        to_return_mask = np.zeros_like(mask)
+        for label in useful_labels:
+            curr_mask = labels == label
+            curr_mask = curr_mask.astype(np.uint8)
+            to_return_mask = cv2.bitwise_or(to_return_mask, curr_mask)
+
+        return to_return_mask
+
+    @staticmethod
+    def find_bounding_rect(processed_mask):
+        contours, _ = cv2.findContours(processed_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        rect_list = []
+        for contour in contours:
+            rect = cv2.minAreaRect(contour)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            box = WrappedImage.organize_corner_points(box)
+            rect_list.append(box)
+
+        return rect_list
+
+    @staticmethod
+    def find_bounding_ellipse(processed_mask):
+        contours, _ = cv2.findContours(processed_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        ellipse_list = []
+
+        for contour in contours:
+            ellipse = cv2.fitEllipse(contour)
+            ellipse_list.append(ellipse)
+
+        return ellipse_list
+
+    @staticmethod
+    def organize_corner_points(point_list):
+        clickLocations = np.array(point_list)
+        center_pt = (np.average(clickLocations[:, 0]), np.average(clickLocations[:, 1]))
+
+        toReturn = {}
+
+        for eachPoint in clickLocations:
+            if (eachPoint[0] < center_pt[0]):
+                if (eachPoint[1] < center_pt[1]):
+                    toReturn[0] = eachPoint
+                else:
+                    toReturn[1] = eachPoint
+            else:
+                if (eachPoint[1] > center_pt[1]):
+                    toReturn[2] = eachPoint
+                else:
+                    toReturn[3] = eachPoint
+
+        toReturn = [toReturn[0].tolist(), toReturn[1].tolist(), toReturn[2].tolist(), toReturn[3].tolist()]
+        return toReturn
+
+    @staticmethod
+    def find_end_locations(bound_rect_list):
+        location_list = []
+        for rect in bound_rect_list:
+            left_x = int(np.average([rect[0][0], rect[1][0]]))
+            left_y = int(np.average([rect[0][1], rect[1][1]]))
+            right_x = int(np.average([rect[2][0], rect[3][0]]))
+            right_y = int(np.average([rect[2][1], rect[3][1]]))
+            location_list.append([(left_x, left_y), (right_x, right_y)])
+
+        return location_list
+
+
+class ColorSilderGroup:
+
+    def __init__(self, tk_root, command):
+        self.hue_slider = Scale(tk_root, from_=0, to=179, orient=HORIZONTAL, label="Upper Hue")
+        self.sat_slider = Scale(tk_root, from_=0, to=255, orient=HORIZONTAL, label="Upper Saturation")
+        self.val_slider = Scale(tk_root, from_=0, to=255, orient=HORIZONTAL, label="Upper Value")
+
+        self.hue_lower_slider = Scale(tk_root, from_=0, to=179, orient=HORIZONTAL, label="Lower Hue")
+        self.sat_lower_slider = Scale(tk_root, from_=0, to=255, orient=HORIZONTAL, label="Lower Saturation")
+        self.val_lower_slider = Scale(tk_root, from_=0, to=255, orient=HORIZONTAL, label="Lower Value")
+
+        self.button = Button(tk_root, text="OK", command=command)
+
+        self.hue_slider.pack()
+        self.sat_slider.pack()
+        self.val_slider.pack()
+
+        self.hue_lower_slider.pack()
+        self.sat_lower_slider.pack()
+        self.val_lower_slider.pack()
+        self.button.pack()
+
+    def get_slider_values(self):
+        hu = self.hue_slider.get()
+        hl = self.hue_lower_slider.get()
+        su = self.sat_slider.get()
+        sl = self.sat_lower_slider.get()
+        vu = self.val_slider.get()
+        vl = self.val_lower_slider.get()
+
+        return {"Hue Upper": hu, "Hue Lower": hl, "Sat Upper": su, "Sat Lower": sl, "Val Upper": vu, "Val Lower": vl}
 
 
 class ColorPicker:
@@ -64,22 +264,41 @@ class ColorPicker:
     def __init__(self, file_name):
         self.image = WrappedImage(file_name)
         self.tk_root = None
+        self.color_slider_group = None
         self.initialize_GUI()
         self.tk_root.mainloop()
 
     def initialize_GUI(self):
         self.tk_root = Tk()
         self.tk_root.title('HSV value selector')
-        hue_slider = Scale(self.tk_root, from_=0, to=179, orient=HORIZONTAL, label="Upper Hue")
-        sat_slider = Scale(self.tk_root, from_=0, to=255, orient=HORIZONTAL, label="Upper Saturation")
-        val_slider = Scale(self.tk_root, from_=0, to=255, orient=HORIZONTAL, label="Upper Value")
+        self.color_slider_group = ColorSilderGroup(self.tk_root, self.display_image_inrange)
+        self.image.change_to_HSV()
+        self.tk_root.geometry("300x400")
 
+    def display_image_inrange(self):
+        cv2.destroyAllWindows()
+        if self.image.get_colo_space() != "HSV":
+            self.image.change_to_HSV()
+        assert self.image.get_colo_space() == "HSV", "Not in the correct Color space"
+        slider_values = self.color_slider_group.get_slider_values()
 
-        hue_slider.pack()
-        sat_slider.pack()
-        val_slider.pack()
-        self.tk_root.geometry("300x200")
+        if slider_values['Hue Upper'] < slider_values['Hue Lower']:
+            upper_value = (slider_values['Hue Upper'], slider_values['Sat Upper'], slider_values['Val Upper'])
+            upper_value_floor = (0, slider_values['Sat Lower'], slider_values['Val Lower'])
+            lower_value_celling = (180, slider_values['Sat Upper'], slider_values['Val Upper'])
+            lower_value = (slider_values['Hue Lower'], slider_values['Sat Lower'], slider_values['Val Lower'])
+            mask_1 = cv2.inRange(self.image.get_image(), lower_value, lower_value_celling)
+            mask_2 = cv2.inRange(self.image.get_image(), upper_value_floor, upper_value)
+            mask = cv2.bitwise_or(mask_1, mask_2)
+        else:
+            upper_value = (slider_values['Hue Upper'], slider_values['Sat Upper'], slider_values['Val Upper'])
+            lower_value = (slider_values['Hue Lower'], slider_values['Sat Lower'], slider_values['Val Lower'])
+            mask = cv2.inRange(self.image.get_image(), lower_value, upper_value)
 
+        self.image.change_to_BGR()
+        masked_image = cv2.bitwise_and(self.image.get_image(), self.image.get_image(), mask=mask)
+        cv2.imshow('Masked Image', np.hstack((cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR), masked_image)))
+        cv2.waitKey(0)
 
     @staticmethod
     def image_value_tester(hue, saturation, value):
@@ -94,6 +313,14 @@ class ColorPicker:
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
+
 if __name__ == "__main__":
     # ColorPicker.image_value_tester(10, 255, 255)
-    ColorPicker("Actual_Photos/12_17/12_17_gs/2021-12-17_13-37-26.jpg")
+    parser = argparse.ArgumentParser(description='Color range adjustment')
+    parser.add_argument('--photo_dir', type=str, required=True)
+
+    args = parser.parse_args()
+    photo_dir = args.photo_dir
+    # ColorPicker(photo_dir)
+    wrapped_image = WrappedImage(photo_dir)
+    wrapped_image.show_hue_only()
--- main.py
+++ main.py
@@ -2,12 +2,69 @@ import argparse
 
 import numpy as np
 import cv2
+from Needle_with_BG.ColorPicker import WrappedImage
 import glob
 
 clickLocation = []
 
-def find_camera_matrix(images):
 
+class NeedleMarkers:
+
+    def __init__(self, markers):
+        markers = np.array(markers)
+        assert len(markers.shape) == 3, "Incorrect dimension of markers"
+        self._markers = markers
+        self._marker_x = []
+        self._marker_y = []
+        self._f = None
+        self._initialize_markers()
+        self._poly_fit()
+        assert len(self._marker_x)/2 == len(self._markers), "Marker Init failed"
+
+    def _initialize_markers(self):
+
+        for marker_pairs in self._markers:
+            self._marker_x.append(marker_pairs[0, 0])
+            self._marker_x.append(marker_pairs[1, 0])
+            self._marker_y.append(marker_pairs[0, 1])
+            self._marker_y.append(marker_pairs[1, 1])
+
+        self._marker_x = np.array(self._marker_x)
+        self._marker_y = np.array(self._marker_y)
+
+    def get_marker(self):
+        return self._markers
+
+    def get_marker_x(self):
+        return self._marker_x
+
+    def get_marker_y(self):
+        return self._marker_y
+
+    def _poly_fit(self, order=4):
+        f = np.polyfit(self._marker_x, self._marker_y, order)
+        self._f = np.poly1d(f)
+
+    def get_polyfit(self, order=4):
+        self._poly_fit(order)
+        return self._f
+
+    def draw_polyfit(self, img):
+        y_fits = self._f(self._marker_x)
+        y_fits = y_fits.astype(np.int32)
+
+        for i in range(len(y_fits) - 1):
+            img = cv2.line(img, (self._marker_x[i], y_fits[i]), (self._marker_x[i+1], y_fits[i+1]), (255, 0, 255), 2)
+
+        return img
+
+    def draw_polyfit_entire_frame(self, img):
+        pass
+
+
+
+
+def find_camera_matrix(images):
     # boardH = 8
     # boardW = 4
     boardH = 8
@@ -67,6 +124,7 @@ def find_camera_matrix(images):
     cv2.destroyAllWindows()
     return newcameramtx, mtx, dist
 
+
 def correct_image(image_path, original_matrix, distortion_coeff, new_camera_matrix, imshow=False):
     img = cv2.imread(image_path)
     restored = cv2.undistort(img, original_matrix, distortion_coeff, None, new_camera_matrix)
@@ -79,6 +137,7 @@ def correct_image(image_path, original_matrix, distortion_coeff, new_camera_matr
 
     return restored
 
+
 def chooseReferencePoints(image):
     cv2.imshow('Choose two base points', image)
     cv2.setMouseCallback("Choose two base points", getXY)
@@ -86,18 +145,18 @@ def chooseReferencePoints(image):
     cv2.destroyAllWindows()
     show_clicked_points(image)
 
+
 def show_clicked_points(image):
     global clickLocation
     frameCopy = image.copy()
 
     center_1 = clickLocation[0]
     center_2 = clickLocation[1]
-    cv2.circle(frameCopy,(center_1[0], center_1[1]), 2, (0, 0, 255), 1)
+    cv2.circle(frameCopy, (center_1[0], center_1[1]), 2, (0, 0, 255), 1)
     cv2.circle(frameCopy, (center_2[0], center_2[1]), 2, (0, 0, 255), 1)
     cv2.imshow('Selected', frameCopy)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
 
 
 def getXY(event, x, y, flags, param):
@@ -108,25 +167,42 @@ def getXY(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         clickLocation.append([x, y])
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Where are the images located")
-    parser.add_argument("--calibration_dir", type=str, required=True)
+    parser.add_argument("--calibration_dir", type=str)
+    parser.add_argument("--image_dir", type=str, required=True)
     args = parser.parse_args()
-    # image_names = glob.glob('./Actual_Photos/*.JPG')
-    dir = "{}\\*.JPG".format(args.calibration_dir)
-    image_names = glob.glob(dir)
-    images = []
 
-    for each_name in image_names:
-        image = cv2.imread(each_name)
-        images.append(image)
+    wrapped_image = WrappedImage(file_name=args.image_dir)
+    marker_only_image, marker_ends = wrapped_image.color_filter_HSV(upper_bound=(10, 255, 255), lower_bound=(170, 40, 40))
+    wrapped_image.draw_marker_on_image(marker_ends)
 
-    new_cam_matrix, Orig_mtx, distortion_coeff = find_camera_matrix(images)
+    markers = NeedleMarkers(marker_ends)
+    polyfit_drawing = markers.draw_polyfit(wrapped_image.get_image())
+    cv2.imshow('Polyfit', polyfit_drawing)
+    cv2.imshow('test', marker_only_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-    # to_correct_path = 'To_Unwarp.jpg'
-    # restored_image = correct_image(to_correct_path, Orig_mtx, distortion_coeff, new_cam_matrix, False)
-    # chooseReferencePoints(restored_image)
-    # np.save('new_cam_mtx.npy', new_cam_matrix)
-    # np.save('cam_mtx.npy', Orig_mtx)
-    # np.save('dist_coeff.npy', distortion_coeff)
 
+
+
+
+    # # image_names = glob.glob('./Actual_Photos/*.JPG')
+    # dir = "{}\\*.JPG".format(args.calibration_dir)
+    # image_names = glob.glob(dir)
+    # images = []
+    #
+    # for each_name in image_names:
+    #     image = cv2.imread(each_name)
+    #     images.append(image)
+    #
+    # new_cam_matrix, Orig_mtx, distortion_coeff = find_camera_matrix(images)
+    #
+    # # to_correct_path = 'To_Unwarp.jpg'
+    # # restored_image = correct_image(to_correct_path, Orig_mtx, distortion_coeff, new_cam_matrix, False)
+    # # chooseReferencePoints(restored_image)
+    # # np.save('new_cam_mtx.npy', new_cam_matrix)
+    # # np.save('cam_mtx.npy', Orig_mtx)
+    # # np.save('dist_coeff.npy', distortion_coeff)
